d9dcb91d1b3209a922d33d74ba96090598dda830
gfifgfifofich <gfif90000@gmail.com>
2024-10-01 18:46:11 +0300

    kubernetes

--- sql/task.sql
+++
@@ -1,4 +0,0 @@
-SELECT Products.name AS productName, Category.name AS categoryName 
-FROM Products
-LEFT JOIN ProdCat ON Products.id = ProdCat.products_id
-INNER JOIN Category  ON Category.id = ProdCat.category_id;
